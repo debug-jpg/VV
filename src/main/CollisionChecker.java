44eab468e4134c889c97986add4170f06f029249
Renz Christian Brobio <renzhotdog8@gmail.com>
2024-08-09 19:33:45 +0800

    Now you can see the code hehe.

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -1,2 +1,63 @@
-package main;public class CollisionChecker {
+package main;
+
+import entity.Entity;
+
+public class CollisionChecker {
+
+    GamePanel gp;
+    Entity entity;
+
+    public CollisionChecker(GamePanel gp) {
+        this.gp = gp;
+    }
+
+    public void checkTile(Entity entity) {
+        int entityLeftWorldX = entity.worldX + entity.solidArea.x;
+        int entityRightWorldX = entity.worldX + entity.solidArea.x + entity.solidArea.width;
+        int entityTopWorldY = entity.worldY + entity.solidArea.y;
+        int entityBottomWorldY = entity.worldY + entity.solidArea.y + entity.solidArea.height;
+
+        int entityLeftCol = entityLeftWorldX / gp.tileSize;
+        int entityRightCol = entityRightWorldX / gp.tileSize;
+        int entityTopRow = entityTopWorldY / gp.tileSize;
+        int entityBottomRow = entityBottomWorldY / gp.tileSize;
+
+        int tileNum1, tileNum2;
+
+        switch (entity.direction) {
+            case "up":
+                entityTopRow = (entityTopWorldY - entity.speed) / gp.tileSize;
+                tileNum1 = gp.tile.mapTileNum[entityLeftCol][entityTopRow];
+                tileNum2 = gp.tile.mapTileNum[entityRightCol][entityTopRow];
+                if (gp.tile.tile[tileNum1].collision || gp.tile.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+                break;
+            case "down":
+                entityBottomRow = (entityBottomWorldY + entity.speed) / gp.tileSize;
+                tileNum1 = gp.tile.mapTileNum[entityLeftCol][entityBottomRow];
+                tileNum2 = gp.tile.mapTileNum[entityRightCol][entityBottomRow];
+                if (gp.tile.tile[tileNum1].collision || gp.tile.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+                break;
+            case "left":
+                entityLeftCol = (entityLeftWorldX - entity.speed) / gp.tileSize;
+                tileNum1 = gp.tile.mapTileNum[entityLeftCol][entityTopRow];
+                tileNum2 = gp.tile.mapTileNum[entityLeftCol][entityBottomRow];
+                if (gp.tile.tile[tileNum1].collision || gp.tile.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+                break;
+            case "right":
+                entityRightCol = (entityRightWorldX + entity.speed) / gp.tileSize;
+                tileNum1 = gp.tile.mapTileNum[entityRightCol][entityBottomRow];
+                tileNum2 = gp.tile.mapTileNum[entityRightCol][entityTopRow];
+                if (gp.tile.tile[tileNum1].collision || gp.tile.tile[tileNum2].collision) {
+                    entity.collisionOn = true;
+                }
+                break;
+        }
+    }
+
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,2 +1,107 @@
-package main;public class GamePanel {
+package main;
+
+import java.awt.*;
+import javax.swing.JPanel;
+import tile.TileManager;
+
+import entity.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    //----------SCREEN----------//
+    final int originalTileSize = 16;
+    final int scale = 3;
+
+    public final int tileSize = originalTileSize * scale;
+    public final int maxScreenCol = 16;
+    public final int maxScreenRow = 12;
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+    //----------SCREEN----------//
+
+    //----------WORLD----------//
+    public final int maxWorldCol = 50;
+    public final int maxWorldRow = 50;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
+    //----------WORLD----------//
+
+    int FPS = 60;
+
+    // Initiate
+    public CollisionChecker hitbox = new CollisionChecker(this);
+    TileManager tile = new TileManager(this);
+    KeyHandler key = new KeyHandler();
+    Thread gameThread;
+    public Player player = new Player(this, key);
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(key);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+            delta += (currentTime - lastTime) / drawInterval;
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+
+        }
+    }
+
+    public void update() {
+        player.update();
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        tile.draw(g2);
+        player.draw(g2);
+
+        g2.dispose();
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
