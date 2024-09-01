12f74ddc731166be8c76034237f6826960f02dbb
Renz Christian Brobio <renzhotdog8@gmail.com>
2024-09-01 17:49:05 +0800

    Updated EventHandler

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -7,59 +7,95 @@ import java.awt.*;
 public class EventHandler {
 
     GamePanel gp;
-    Rectangle eventRect;
-    int eventRectDefaultX, eventRectDefaultY;
+    EventRect eventRect[][];
+
+    int previousEventX, previousEventY;
+    boolean canTouchEvent = true;
 
     public EventHandler(GamePanel gp) {
         this.gp = gp;
 
-        eventRect = new Rectangle(23, 23, 2, 2);
-        eventRectDefaultX = eventRect.x;
-        eventRectDefaultY = eventRect.y;
+        eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+
+        int col = 0;
+        int row = 0;
+
+        while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+
+            eventRect[col][row] = new EventRect();
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = 23;
+            eventRect[col][row].height = 23;
+            eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
+            eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
+            col++;
+            if (col == gp.maxWorldCol) {
+                col = 0;
+                row++;
+            }
+
+        }
 
     }
 
     public void checkEvent() {
+        //If player is > 1 tile then check
+        int xDistance = Math.abs(gp.player.worldX - previousEventX);
+        int yDistance = Math.abs(gp.player.worldY - previousEventY);
+        int distance = Math.max(xDistance, yDistance);
+        if (distance > gp.tileSize) {
+            canTouchEvent = true;
+        }
 
-        if (hit(24, 9, "right") == true) { damagePit(gp.dialogState); }
-        if (hit(23, 7, "up") == true) { healingPool(gp.dialogState); }
+        if (canTouchEvent) {
+            if (hit(24, 9, "right") == true) { damagePit(24, 9, gp.dialogState); }
+            if (hit(23, 7, "up") == true) { healingPool(23, 7, gp.dialogState); }
+        }
+
         
     }
 
-    public boolean hit(int eventCol, int eventRow, String reqDirection) {
+    public boolean hit(int col, int row, String reqDirection) {
 
         boolean hit = false;
 
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        eventRect.x = eventCol * gp.tileSize + eventRect.x;
-        eventRect.y = eventRow * gp.tileSize + eventRect.y;
+        eventRect[col][row].x = col * gp.tileSize + eventRect[col][row].x;
+        eventRect[col][row].y = row * gp.tileSize + eventRect[col][row].y;
 
-        if (gp.player.solidArea.intersects(eventRect)) {
+        if (gp.player.solidArea.intersects(eventRect[col][row]) && eventRect[col][row].eventDone == false) {
             if (gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")) {
                 hit = true;
+
+                previousEventX = gp.player.worldX;
+                previousEventY = gp.player.worldY;
             }
         }
 
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        eventRect.x = eventRectDefaultX;
-        eventRect.y = eventRectDefaultY;
+        eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
+        eventRect[col][row].y = eventRect[col][row].eventRectDefaultY;
 
 
         return hit;
 
     }
 
-    public void damagePit(int gameState) {
+    public void damagePit(int col, int row, int gameState) {
 
         gp.gameState = gameState;
         gp.ui.currentDialog = "You fall to my...";
         gp.player.life -= 1;
+//        eventRect[col][row].eventDone = true;
+        canTouchEvent = false;
 
     }
 
-    public void healingPool(int gameState) {
+    public void healingPool(int col, int row, int gameState) {
 
         if (gp.key.enter == true) {
             gp.gameState = gameState;
---
+++ src/main/EventRect.java
@@ -0,0 +1,11 @@
+package main;
+
+import java.awt.*;
+
+@SuppressWarnings("all")
+public class EventRect extends Rectangle {
+
+    int eventRectDefaultX, eventRectDefaultY;
+    boolean eventDone = false;
+
+}
